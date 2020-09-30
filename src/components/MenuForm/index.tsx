d52b6719688fa75aada8cb3c3960d689c3b19ed2
Victor Brambati <victorbrambati@gmail.com>
2020-09-30 20:35:54 -0300

    Finished

---
+++ src/components/MenuForm/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Container, Navigation, DropboxLogo, Form } from './styles';
+
+const MenuForm: React.FC = () => {
+  function handleToggle() {
+    if (window.toggleActiveMenu) window.toggleActiveMenu();
+  }
+  return (
+    <Container>
+      <Navigation>
+        <h1>
+          <DropboxLogo />
+          <span>Dropbox</span>
+        </h1>
+
+        <button className="action--close" onClick={handleToggle}>
+          ✕
+        </button>
+      </Navigation>
+
+      <Form>
+        <span className="title">Registre-se</span>
+        <span className="subtitle">preencha o formulário abaixo</span>
+
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="sobrenome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button>Prosseguir</button>
+
+        <span className="terms">
+          Esta página está sujeita á Politica de privacidade e aos termos de
+          serviço.
+        </span>
+      </Form>
+    </Container>
+  );
+};
+
+export default MenuForm;
